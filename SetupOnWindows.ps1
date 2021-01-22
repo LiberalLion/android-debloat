ba47a010746b884ffcc82142d6e0a7c7b5812d41
Pranav Kulkarni <pranavkulkarni195@gmail.com>
2021-01-22 15:01:07 +0530

    ☕ Improved

--- SetupOnWindows.ps1
+++ SetupOnWindows.ps1
@@ -32,42 +32,30 @@ function installchoco {
     Write-Host "Installing required Tools quickly."
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     choco install chocolatey-core.extension -y
-
+    refreshenv
 }
 
 function setupall { 
 
+    refreshenv
     Write-Host "Installing drivers for your Phone"
     choco install git -y
-    choco install adb -y
-    Write-Host "Done. Now Installing the program!"
-    #Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.30.0.windows.1/Git-2.30.0-64-bit.exe C:\GitSetup.exe
-    #curl -sSL https://git.io/JtvVG | bash
-    $wshell.Popup("Now Please connect your phone via USB cable and enable USB DEBUGGING from Developer Settings.",0,"I Know",0x0)    
-    $wshell.Popup("When asked, Allow the access on your phone and tick the box to always grant the privileges.",0,"Done",0x0) 
-    $wshell.Popup("Now right click on your desktop background and click 'Git Bash' or something similar. Wait for around a minute for it to load completely. then type the following in it without the quotes. 'curl -sSL https://git.io/JtvKJ | bash'",0,"Sure",0x0)    
-    
+    choco install adb -y    
+    refreshenv
 }
 
+function runapp { 
 
-# function runtv {
-#     & 'C:\Program Files (x86)\TeamViewer\TeamViewer.exe'
-#     Invoke-WebRequest https://raw.githubusercontent.com/gamerhat18/win10script/master/essentialsInstall.ps1 C:\essentialsInstall.ps1
-# }
-
-
-# iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/JtvVG'))
+    refreshenv
+    Write-Host "Done. Now Installing the program!"
+    cd C:\users\{$env:username}\Documents\
+    git clone https://github.com/gamerhat18/android-debloat
+    $wshell.Popup("Now Please connect your phone via USB cable and enable USB DEBUGGING from Developer Settings.",0,"I Know",0x0)    
+    $wshell.Popup("When asked, Allow the access on your phone and tick the box to always grant the privileges.",0,"Done",0x0) 
+    $wshell.Popup("Now right click on your desktop background and click 'Git Bash' or something similar. Wait for around a minute for it to load completely. then type the following in it without the quotes. 'curl -sSL https://git.io/JtvKJ | bash'",0,"Sure",0x0)
+    Write-Host "Installing drivers for your Phone"
+}
 
 installchoco
 
 setupall
-
-# Chris Titus' Debloat Script command given below for quick copy-paste.
-# iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/JJ8R4'))
-
-# My scripts
-# https://cybrdise.com/sakshi/one
-
-
-## RUN THIS SAME BUT ONLY TEAMVIEWER AUTOMATICALLY.
-## iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/Jtvit'))
